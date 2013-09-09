c6a4139fb38cb0c5753914254694ea6e6207aa8c
alicefranco <franco.lice@gmail.com>
2013-09-09 18:19:35 -0300

    Stepper motor modules
    Stepper Motor is complete, tested and working. It includes a program for
    each arduino and python control.

---
+++ arduino/StepperMotor/arduinosteppermotor.ino
@@ -0,0 +1,30 @@
+#include <Stepper.h>
+
+
+const int stepsPerRevolution = 64;
+Stepper myStepper(stepsPerRevolution, 11,9,10,8);            
+
+
+void setup() {
+  myStepper.setSpeed(200);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()) {
+      char c = Serial.read();
+      if (c == 'F') {
+        for (int i = 0; i < 32; i++)
+        {
+          myStepper.step(1);
+        }
+      }
+      else if (c == 'B') {
+        for (int i = 0; i < 32; i++)
+        {
+          myStepper.step(-1);
+        }
+      }
+  } 
+}
+
