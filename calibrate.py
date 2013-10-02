5b24993fe949e14d858fd9fc4cbc19f71faf4206
asaphemagno <asaphemagno@hotmail.com>
2013-10-02 18:05:31 -0300

    Update calibrate.py

--- calibrate.py
+++ calibrate.py
@@ -2,8 +2,9 @@ import cv2
 import numpy
 
 '''	Calcula parAmetros intrInsecos e extrInsecos de uma camera 
-a partir de uma imagem com resoluCAo arbitrAria de um tabuleiro de xadrez 
-com 10X7 quadrados de largura e altura, respectivamente, obtida a partir da mesma camera. '''
+a partir de imagens com resoluCAo arbitrAria de um padrAo de calibraCAo 
+com 8X9 cIrculos de largura e altura, respectivamente, obtida a partir da mesma camera.
+E realiza a remoCAo das distorCOes radial e tangencial de imagens obtidas com essa camera '''
 
 ''' VariAveis Globais '''
 camera_matrix = []
@@ -11,92 +12,70 @@ dist_coefs = []
 rvecs = [] 
 tvec = []
 calibrated = False
+num_patterns = None
+img_patterns = []
+pattern_size = (8,9)
+cauht_patterns = False
+image_points = []
+width = 0
+height = 0 
 
 
-''' Realiza a correCAo da distorCAo radial e tangencial e retorna um tuple 
-indicando o sucesso ou nAo da calibraCAo da camera assim 
-como a imagem do tabuleiro de chadrez corrigida '''
-def calibrate(src_image_name): 
-	
+''' Calcula parAmetros intrInsecos e extrInsecos da camera '''
+def calibrate(): 
+
 	global camera_matrix
 	global dist_coefs
 	global rvecs 
 	global tvec 
 	global calibrated
+	global pattern_size
+	global cauht_patterns 
+	global image_points
+	global width
+	global height
+	global num_patterns
 	
-	''' Inicializa algumas variAveis necessArias para obtenCAo das cordenadas dos
-	pontos dos objetos '''
-	pattern_size = (9,6)
-	square_size = 1.0
-	object_points = []
-	image_points = []
-	width = 0
-	height = 0 
-	
-	''' LE a imagem de entrada '''
-	src_image = cv2.imread(src_image_name)
-	
-	''' ObtEm a resoluCAo da imagem '''
-	height, width = src_image.shape[:2]
-	''' Gera as cordenadas dos pontos dos objetos '''
+	if not cauht_patterns:
+		print 'The parameters are not yet caught.'
+		return 0
+	''' Inicializa uma variAvel necessAria para obtenCAo das cordenadas dos
+	pontos dos objeto '''
+	object_points = []	
+	''' Gera as cordenadas dos pontos dos objeto '''
 	pattern_points = numpy.zeros( ( numpy.prod(pattern_size) , 3) , numpy.float32 ) 
 	pattern_points [:,:2] = numpy.indices(pattern_size).T.reshape(-1,2)
-	pattern_points *= square_size
-	''' Realiza a converSAo da imagem de entrada para Escala de cinza com profundidade de 8 bits '''
-	gray_image = cv2.cvtColor(src_image, cv2.COLOR_RGB2GRAY)
-	
-	''' Encontra as cordenadas dos cantos dos quadrados internos do tabuleiro de xadrez '''
-	val,corners = cv2.findChessboardCorners(gray_image , pattern_size) 
-	
-	''' Refina as cordenadas dos cantos dos quadrados internos do tabuleiro de xadrez '''
-	term = ( cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_COUNT, 30, 0.1 )
-	cv2.cornerSubPix(gray_image, corners, (5, 5), (-1, -1), term)
-	
-	''' Testa se foi possIvel encontrar as cordenadas dos cantos dos quadrados internos	
-	do tabuleiro de xadrez. Se foi possIvel, prossegue com a calibraCAo. SenAo, 
-	interrompe a execuCAo e exibe mensagem de erro'''
-	if val:
-		
-		''' Inclui as cordenadas dos pontos dos objetos e as da imagem depois de remoldadas, em listas individuais '''
-		image_points.append(corners.reshape(-1,2))
+	''' Inclui as cordenadas dos pontos dos objeto em uma lista individual '''
+	counter0 = num_patterns 
+	while(counter0):
 		object_points.append(pattern_points)
-		''' Calcula parAmetros intrInseco e extrInsecos da camera alEm dos vetores de rotaCAo e translaCAo '''
-		rms, camera_matrix, dist_coefs, rvecs, tvec = cv2.calibrateCamera( object_points, image_points,(width, height) )
-		''' Reliza a correCAo da distorCAo da imagem fornecida. Gerando dois mapas a partir dos parAmetros dados e 
-		remapeando cada pixel da imagem de entrada para a sua verdadeira posiCAo na imagem de saIda atravEs desses mesmos mapas '''
-		dst_image = cv2.undistort(src_image, camera_matrix, dist_coefs)
-		'''Muda o status da camera para calibrada'''
-		calibrated = True
-		''' Exibe mensagem de erro caso nAo tenha sido possIvel encontrar as cordenadas dos cantos dos quadrados internos	
-		do tabuleiro de xadrez '''
-	else:
-		
-		print "The Program can not correct the image"
+		counter0 = counter0 -1
+	''' Calcula parAmetros intrInsecos e extrInsecos da camera alEm dos vetores de rotaCAo e translaCAo '''
+	rms, camera_matrix, dist_coefs, rvecs, tvec = cv2.calibrateCamera( object_points, image_points,(width, height) )
+	'''Muda o status da camera para calibrada'''
+	calibrated = True
 	
-		''' Retorna a imagem corrigida e o status da operaCAo '''
-	return val, dst_image
-	
-	
-	''' Corrige as distorCOes radial e tangencial da imagem de entrada e grava o resultado em um arquivo de saIda cujo diretOrio E dado '''
+
+''' Corrige as distorCOes radial e tangencial da imagem de entrada e grava o resultado em um arquivo de saIda cujo diretOrio E dado '''
 def correct_image(src_name,dst_name):
-	
+
 	global camera_matrix
 	global dist_coefs
 	global calibrated
-	
+
 	''' Verifica se a camera jA foi calibrada '''
 	if not calibrated:
 		print 'Camera has not been calibrated'
 		return False
 	''' LE a imagem de entrada '''
 	in_image = cv2.imread(src_name)
-	
+
 	''' Corrige as distorCOes da imgem '''
 	out_image = cv2.undistort(in_image, camera_matrix, dist_coefs)
-	
+
 	''' Grava a imagem resultante '''
 	ok = cv2.imwrite(dst_name,out_image)
-	
+
 	return ok		
 
 ''' Retorna parAmetros intrInsecos e extrInsecos de uma camera '''	
@@ -105,5 +84,80 @@ def get_cam_parameters():
 	''' Verifica se a camera jA foi calibrada '''
 	if not calibrated:
 		print 'Camera has not been calibrated'
-		
+
 	return camera_matrix, dist_coefs, rvecs, tvec
+
+def get_patterns(cam_id, number_patterns):
+
+	global pattern_size
+	global num_patterns
+	global image_points
+	global height
+	global width
+	global cauht_patterns
+	
+	capture = cv2.VideoCapture(cam_id)
+		
+	cv2.namedWindow('Cam',1)
+	capture.set(3,1280) 
+	capture.set(4,960)
+	counter = 0
+		
+	while (number_patterns > counter):
+		
+		ok, img = capture.read()
+		im = img
+		''' ObtEm a resoluCAo das imagens '''
+		height, width = img.shape[:2]
+		''' Encontra as cordenadas dos centros dos circulos da grade '''
+		pattern_found, centers = cv2.findCirclesGrid(img,pattern_size,flags = cv2.CALIB_CB_SYMMETRIC_GRID)
+		cv2.drawChessboardCorners(img, pattern_size, centers, pattern_found)
+		cv2.imshow('Cam',img)
+		val = cv2.waitKey(1)
+		if (val == 27):
+			print'Capture Process Aborted'
+			break
+		if ( (val == 99) and ( pattern_found )):
+			img_patterns.append(im)
+			image_points.append(centers.reshape(-1,2))
+			print'%snd Image Captured.' % (counter + 1)
+			counter = counter + 1	
+	num_patterns = number_patterns
+	cauht_patterns = True
+	cv2.destroyWindow('Cam')
+
+def print_cam_parameters():	
+
+	global camera_matrix 
+	global dist_coefs
+	
+	print'\n'
+	print'Camera Matrix: \t', camera_matrix 
+	print'\n'
+	print'Distortion Coeficients: \t', dist_coefs
+	
+def show_res_calib():
+
+	global calibrated
+	global img_paterns
+	global camera_matrix
+	global dist_coefs
+	
+	if not calibrated:
+		print 'Camera has not been calibrated'
+		return 0
+	cv2.namedWindow('1nd Image',1)
+	dst_image = cv2.undistort(img_patterns[0], camera_matrix, dist_coefs)
+	
+	cv2.imshow('1nd Image', dst_image)
+	cv2.waitKey(0)
+		
+	
+'''if __name__ == '__main__':
+	
+	get_patterns(0,20)
+	calibrate()
+	correct_image('d:/d/grade_o.jpg','d:/d/grade_c.jpg')
+	print_cam_parameters()	
+	show_res_calib()'''
+	
